d2d558ae4f49aa5b89bfca570d9adabda11699af
YevheniiMakar <maggogg88@gmail.com>
2021-07-16 20:02:16 +0300

    compile_maven done

---
+++ unit_2_compile/compile_maven/run.bat
@@ -0,0 +1,4 @@
+call mvn clean package
+call java -jar target/compile_maven.jar
+cmd /k
+
---
+++ unit_2_compile/compile_maven/src/main/java/com/yevheniimakar/compile_maven/Main.java
@@ -0,0 +1,12 @@
+package com.yevheniimakar.compile_maven;
+
+import com.yevheniimakar.compile_maven.test.Test1;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("console compile class Main");
+        new Test1().test();
+
+    }
+
+}
