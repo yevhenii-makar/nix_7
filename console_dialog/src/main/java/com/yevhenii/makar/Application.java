e3594924dd18b7aec2c9e0c1a7a7b848cd145885
YevheniiMakar <maggogg88@gmail.com>
2021-08-16 23:08:30 +0300

    _

--- console_dialog/src/main/java/com/yevhenii/makar/Application.java
+++ console_dialog/src/main/java/com/yevhenii/makar/Application.java
@@ -2,7 +2,6 @@ package com.yevhenii.makar;
 
 public class Application {
     public static void run(Class<?> clazz){
-
         ConsoleDialog consoleDialog = new ConsoleDialog(clazz.getPackage().getName());
     }
 }
